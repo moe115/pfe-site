dfa30c406d722676d3a17b448e64417726a52b29
moudi115 <moudihmdn@gmail.com>
2023-06-12 16:22:07 +0300

    fnf

--- imgclassification/src/Home.js
+++ imgclassification/src/Home.js
@@ -1,15 +1,75 @@
-import React from 'react'
-
+import React from 'react';
+import './home.css';
 function Home() {
   return (
     <div>
         
 
-
-<h1>welcome to our website were you classify images like flowers and animals using deep learning
-</h1>
-
-
+        <div className="scene">
+  <div className="box"></div>
+  <div className="box-two"></div>
+  <div className="flap"></div>
+  <div className="flap-shadow"></div>
+  <div className="shadow"></div>
+  <div className="detail"></div>
+  <div className="floor"></div>
+  <div className="detail-box"></div>
+  <div className="cat-one-lh"></div>
+  <div className="cat-one-rh"></div>
+  <div className="cat-one-le"></div>
+  <div className="cat-one-lec"></div>
+  <div className="cat-one-lecc"></div>
+  <div className="cat-one-center"></div>
+  <div className="cat-one-re"></div>
+  <div className="cat-one-rec"></div>
+  <div className="cat-one-recc"></div>
+  <div className="cat-one-eye"></div>
+  <div className="cat-one-eye-two"></div>
+  <div className="cat-one-nose"></div>
+  <div className="cat-one-whisker"></div>
+  <div className="cat-one-whisker-two"></div>
+  <div className="cat-one-paw"></div>
+  <div className="cat-one-paw-two"></div>
+  <div className="cat-paw-detail-r"></div>
+  <div className="cat-paw-detail-r-two"></div>
+  <div className="cat-paw-detail-l"></div>
+  <div className="cat-paw-detail-l-two"></div>
+  <div className="cat-two-lh"></div>
+  <div className="cat-two-rh"></div>
+  <div className="cat-two-le"></div>
+  <div className="cat-two-lec"></div>
+  <div className="cat-two-lecc"></div>
+  <div className="cat-two-center"></div>
+  <div className="cat-two-re"></div>
+  <div className="cat-two-rec"></div>
+  <div className="cat-two-recc"></div>
+  <div className="cat-two-eye"></div>
+  <div className="cat-two-eye-two"></div>
+  <div className="cat-two-nose"></div>
+  <div className="cat-two-whisker"></div>
+  <div className="cat-two-whisker-two"></div>
+  <div className="paint-job"></div>
+  <div className="ear-paint"></div>
+  <div className="ear-paint-two"></div>
+  <div className="belly"></div>
+  <div className="coat"></div>
+  <div className="coat-two"></div>
+  <div className="tail-one"></div>
+  <div className="tail-one-detail"></div>
+  <div className="tail-one-detail-two"></div>
+  <div className="tail-detail-hide"></div>
+  <div className="tail-two"></div>
+  <div className="tail-two-detail"></div>
+  <div className="tail-two-detail-two"></div>
+  <div className="tail-two-detail-hide"></div>
+  <div className="tail-two-detail-hide-two"></div>
+  <div className="detail-two"></div>
+  <div className="detail-three"></div>
+  <div className="detail-four"></div>
+  <div className="star"></div>
+  <div className="star-two"></div>
+  <div className="final-flap"></div>
+</div>
     
     </div>
   )
